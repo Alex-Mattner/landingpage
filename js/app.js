c0db9e0c5b23e6a47aeb1fcf3e3f01664c10b685
Alexander Mattner <alexander.mattner@otto-schneider.de>
2021-04-11 18:11:11 +0200

    buildNavBar function finished

--- js/app.js
+++ js/app.js
@@ -26,7 +26,6 @@
 */
 
 
-
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -34,11 +33,38 @@
 */
 
 // build the nav
+const navBar = document.getElementById('navbar__list');
+//console.log(navBar);
+function buildNavBar() {
+    const sections = document.querySelectorAll('section');
+    //console.log(sections);
+    for (section of sections) {
+        const idOfSection = section.id;
+        const dataNavOfSection = section.dataset.nav;
+      
+        //console.log(section);
+        const newListItem = document.createElement('li');
+        newListItem.innerHTML = `<a class="menu__link" href="#${idOfSection}">${dataNavOfSection}</a>`
+        navBar.appendChild(newListItem);
+    }
+}
 
+buildNavBar();
 
 // Add class 'active' to section when near top of viewport
+window.addEventListener('scroll', setActiveClass);
 
+function setActiveClass(event){
+    console.dir(event);
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+}
 
+/* let sections = document.querySelectorAll('section')
+function getActiveClass(sections) {
+    
+    console.log(Math.floor(sections.getBoundingClientRect().top));
+}; */
 // Scroll to anchor ID using scrollTO event
 
 
